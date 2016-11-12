cded2e1b8aee90bba6f49eaee2ed277463b71794
Matthew Violet <mattviolet123@gmail.com>
2016-11-12 12:06:29 -0600

    Auto Login For Facebook Completed

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,14 +1,21 @@
-
-console.log("Hey");
-
 function fillFields() {
 	console.log("function is being called");
 	var emailBox = document.getElementById('email');
 	emailBox.value = "mjv359@aol.com";
+
+	var passBox = document.getElementById('pass');
+	passBox.value = "michaelgarvey";
 }
+
+function logIn() {
+	var logIn = document.getElementById('u_0_n');
+	logIn.click();
+}
+
 
 function handleError() {
 	console.log('uh oh');
 }
 
-fillFields("Yup");
+fillFields();
+logIn();
