bc1611f3d72f5f1503323e1b8595cb92b2c72519
Ridoy Majumdar <majumdar.ridoy@gmail.com>
2016-11-13 08:11:13 -0600

    Prettify popup

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,22 +1,65 @@
 var serverURL = 'https://fd82060c.ngrok.io';
 function showPopup(callbackFunc) {
-	var popUp = document.createElement('div');
-	console.log(popUp);
+    $('#datboiContainer').remove()
+    $('#datboi').remove();
+    $('#browserLockButton').remove();
+    $('#browserLockVid').remove();
 
+    var datboiContainer = document.createElement('div')
+	var popUp = document.createElement('div');
+    var header = document.createElement('h1');
+    var hr = document.createElement('hr');
+
+    $(header).css({
+        'padding': '30px 0 0',
+        'margin-bottom': '15px',
+        'text-align': 'center'
+    });
+
+    $(hr).css({
+        'border': '0',
+        'border-top': '1px solid #e2e2e2',
+        'height': '0',
+        'margin-left': '40px',
+        'margin-right': '40px',
+        'padding': '10px'
+    });
+
+    header.innerText = 'Sign In With Your Face';
+
+	datboiContainer.setAttribute('id', 'datboiContainer');
 	popUp.setAttribute('id', 'datboi');
 
 	//$(popUp).appendTo('body');
 	$(popUp).css({
-		"height": '300px',
-		"width": '300px',
-		"background-color": 'white'
+		"width": '540px',
+		"height": '540px',
+        "border-radius": '8px',
+        "margin-left": 'auto',
+        "margin-right": 'auto',
+        "margin-top": "40px",
+		"background-color": 'rgb(251, 251, 251)'
 	});
 
 	var captureButton = document.createElement('button');
+	captureButton.setAttribute('id', 'browserLockButton');
 	captureButton.innerHTML = "Capture";
-
+    $(captureButton).css({
+        'border-radius': '8px',
+        'background-color': '#7bb9ef',
+        'padding': '15px',
+        'margin-top': '15px',
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+        'width': '100px',
+        'display': 'block',
+        'color': 'white',
+        'font-size': '15px',
+        'font-weight':'900'
+    });
 
 	var video = document.createElement("video");
+	video.setAttribute('id', 'browserLockVid');
 	video.setAttribute('autoplay', 'true');
     navigator.getUserMedia({video: true}, handleVideo, videoError);
 
@@ -24,12 +67,22 @@ function showPopup(callbackFunc) {
 	function handleVideo(stream) {
 		console.log('dat boi boiii');
 	    video.src = window.URL.createObjectURL(stream);
-	    console.log(popUp);
 
+        popUp.appendChild(header);
+        popUp.appendChild(hr);
     	popUp.appendChild(video);
         popUp.appendChild(captureButton);
         popUp.appendChild(text);
-		document.body.appendChild(popUp);
+        datboiContainer.appendChild(popUp);
+        $(datboiContainer).css({
+            'width': '100%',
+            'height': '100%',
+            'background-color': 'rgba(0,0,0,0.5)',
+            'z-index': '137193',
+            'position': 'fixed'
+        });
+        firstChild = document.body.childNodes[0]
+		document.body.insertBefore(datboiContainer, firstChild);
 	}
 
 	function videoError(e) {
@@ -37,8 +90,11 @@ function showPopup(callbackFunc) {
 	}
 
 	$(video).css({
-		"height": '300px',
-		"width": '300px'
+        "width": '480',
+		"height": '360',
+        'display': 'block',
+        'margin-left': 'auto',
+        'margin-right': 'auto',
 	});
 
 
